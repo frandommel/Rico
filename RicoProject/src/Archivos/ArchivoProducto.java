7527ada07da8db876132510feb9f1534ed93f099
Francisco <pancho.dommel@gmail.com>
2020-06-10 12:30:52 -0300

    Archivos Productos
    Se agrego todos los archivos correspondientes a los productos

--- RicoProject/src/Archivos/ArchivoProducto.java
+++ RicoProject/src/Archivos/ArchivoProducto.java
@@ -12,8 +12,12 @@ import java.util.ArrayList;
 
 import javax.swing.text.StyledEditorKit.ForegroundAction;
 
+import claseProductos.Bebida;
 import claseProductos.Combo;
+import claseProductos.Ensalada;
+import claseProductos.Guarnicion;
 import claseProductos.Hamburguesa;
+import claseProductos.Pancho;
 
 public class ArchivoProducto 
 {
@@ -23,7 +27,7 @@ public class ArchivoProducto
 	}
 	
 	//** SE AGREGA COMBOS A UN ARCHIVO **//
-	public void agregarCombo(Combo combo) 
+	public void agregarCombo(ArrayList <Combo> combo) 
 	{
 		FileOutputStream file = null;
 		try 
@@ -50,17 +54,17 @@ public class ArchivoProducto
 	public void leerCombo()
 	{
 		FileInputStream file = null;
-		Combo combo = null;
+		ArrayList<Combo> combo;
 		
 		try {
 			
 			file = new FileInputStream("combos.bin");
 			ObjectInputStream object = new ObjectInputStream(file);
-			while((combo = (Combo) object.readObject()) != null) 
+			
+			while((combo = (ArrayList <Combo>) object.readObject()) != null) 
 			{
 					System.out.println(combo.toString());	
 			}
-			
 			object.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -73,7 +77,7 @@ public class ArchivoProducto
 	}
 	
 	//** SE AGREGA HAMBURGUESA A UN ARCHIVO **//
-	public void agregarHamburguesa(Hamburguesa hambu) 
+	public void agregarHamburguesa(ArrayList <Hamburguesa> hambu) 
 	{
 		FileOutputStream file = null;
 		try 
@@ -99,15 +103,15 @@ public class ArchivoProducto
 	public void leerHamburguesa()
 	{
 		FileInputStream file = null;
-		Hamburguesa hambu = null;
+		ArrayList <Hamburguesa> hambu;
 		
 		try {
 			
 			file = new FileInputStream("hamburguesa.bin");
 			ObjectInputStream object = new ObjectInputStream(file);
-			while((hambu = (Hamburguesa) object.readObject()) != null) 
+			while((hambu = (ArrayList<Hamburguesa>) object.readObject()) != null) 
 			{
-					System.out.println(hambu.toString());	
+					System.out.println("\n" + hambu.toString());	
 			}
 			
 			object.close();
@@ -120,6 +124,203 @@ public class ArchivoProducto
 		}
 
 	}
+	
+	//** SE AGREGA PANCHO A UN ARCHIVO **//
+	public void agregarPancho(ArrayList <Pancho> pancho) 
+	{
+		FileOutputStream file = null;
+		try 
+		{
+			file = new FileOutputStream("pancho.bin");
+			ObjectOutputStream object = new ObjectOutputStream(file);
+			
+			object.writeObject(pancho);
+			object.close();
+			
+		}catch(IOException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				file.close();
+			}catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+	//** SE LEE PANCHO DE UN ARCHIVO **//
+	public void leerPancho()
+	{
+		FileInputStream file = null;
+		ArrayList <Pancho> hambu;
+		
+		try {
+			
+			file = new FileInputStream("pancho.bin");
+			ObjectInputStream object = new ObjectInputStream(file);
+			while((hambu = (ArrayList<Pancho>) object.readObject()) != null) 
+			{
+					System.out.println("\n" + hambu.toString());	
+			}
+			
+			object.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	//** SE AGREGA ENSALADA A UN ARCHIVO **//
+	public void agregarEnsalada(ArrayList <Ensalada> ensalada) 
+	{
+		FileOutputStream file = null;
+		try 
+		{
+			file = new FileOutputStream("ensalada.bin");
+			ObjectOutputStream object = new ObjectOutputStream(file);
+			
+			object.writeObject(ensalada);
+			object.close();
+			
+		}catch(IOException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				file.close();
+			}catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+	//** SE LEE ENSALADA DE UN ARCHIVO **//
+	public void leerEnsalada()
+	{
+		FileInputStream file = null;
+		ArrayList <Ensalada> ensalada;
+		
+		try {
+			
+			file = new FileInputStream("ensalada.bin");
+			ObjectInputStream object = new ObjectInputStream(file);
+			while((ensalada = (ArrayList<Ensalada>) object.readObject()) != null) 
+			{
+					System.out.println("\n" + ensalada.toString());	
+			}
+			
+			object.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	//** SE AGREGA GUARNICION A UN ARCHIVO **//
+	public void agregarGuarnicion(ArrayList <Guarnicion> guarnicion) 
+	{
+		FileOutputStream file = null;
+		try 
+		{
+			file = new FileOutputStream("guarnicion.bin");
+			ObjectOutputStream object = new ObjectOutputStream(file);
+			
+			object.writeObject(guarnicion);
+			object.close();
+			
+		}catch(IOException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				file.close();
+			}catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+	//** SE LEE GUARNICION DE UN ARCHIVO **//
+	public void leerGuarnicion()
+	{
+		FileInputStream file = null;
+		ArrayList <Guarnicion> guarnicion;
+		
+		try {
+			
+			file = new FileInputStream("guarnicion.bin");
+			ObjectInputStream object = new ObjectInputStream(file);
+			while((guarnicion = (ArrayList<Guarnicion>) object.readObject()) != null) 
+			{
+					System.out.println("\n" + guarnicion.toString());	
+			}
+			
+			object.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	//** SE AGREGA BEBIDAS A UN ARCHIVO **//
+	public void agregarBebida(ArrayList <Bebida> bebida) 
+	{
+		FileOutputStream file = null;
+		try 
+		{
+			file = new FileOutputStream("bebida.bin");
+			ObjectOutputStream object = new ObjectOutputStream(file);
+			
+			object.writeObject(bebida);
+			object.close();
+			
+		}catch(IOException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				file.close();
+			}catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+	//** SE LEE BEBIDAS DE UN ARCHIVO **//
+	public void leerBebida()
+	{
+		FileInputStream file = null;
+		ArrayList <Bebida> bebida;	
+		try {
+			
+			file = new FileInputStream("bebida.bin");
+			ObjectInputStream object = new ObjectInputStream(file);
+			while((bebida = (ArrayList<Bebida>) object.readObject()) != null) 
+			{
+					System.out.println("\n" + bebida.toString());	
+			}			
+			object.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	
+	
 	
 	
 	
--- RicoProject/src/paquete/Main.java
+++ RicoProject/src/paquete/Main.java
@@ -20,6 +20,12 @@ public class Main {
 	public static void main(String[] args){
 		
 		ArchivoProducto archiProductos = new ArchivoProducto();
+		ArrayList <Combo> combos = new ArrayList();
+		ArrayList <Hamburguesa> hamburguesas = new ArrayList();
+		ArrayList <Pancho> panchos = new ArrayList();
+		ArrayList <Ensalada> ensaladas = new ArrayList();
+		ArrayList <Guarnicion> guarniciones = new ArrayList();
+		ArrayList <Bebida> bebidas = new ArrayList();
 		
 		Bebida bebidaGaseosa237 = new Bebida("Gaseosa 237cc",60,"Linea Pepsi");
 		Bebida bebidaGaseosa500 = new Bebida("Gaseosa 500cc",70,"Linea Pepsi");
@@ -28,21 +34,43 @@ public class Main {
 		Bebida bebidaPatagonia = new Bebida("Cerveza",165,"Patagonia");
 		Bebida bebidaStellaArtois = new Bebida("Cerveza",130,"Stella Artois");
 		
+		bebidas.add(bebidaGaseosa237);
+		bebidas.add(bebidaGaseosa500);
+		bebidas.add(bebidaLimonada);
+		bebidas.add(bebidaCorona);
+		bebidas.add(bebidaPatagonia);
+		bebidas.add(bebidaStellaArtois);
+		
 
 		Guarnicion guarnicionNuggets = new Guarnicion("Nuggets", 150);
 		Guarnicion guarnicionPapas = new Guarnicion("Papas Fritas", 100);
 		Guarnicion guarnicionRico = new Guarnicion("Papas Rico", 200);
+		
+		guarniciones.add(guarnicionNuggets);
+		guarniciones.add(guarnicionPapas);
+		guarniciones.add(guarnicionRico);
 
 		
 		Ensalada ensaladaUkelele=new Ensalada("Ukelele", 160 , 250);
 		Ensalada ensaladaHulaHula=new Ensalada("Hula Hula", 160 , 180);
 		Ensalada ensaladaAloha=new Ensalada("Aloha", 160 , 210);
 		
+		ensaladas.add(ensaladaUkelele);
+		ensaladas.add(ensaladaHulaHula);
+		ensaladas.add(ensaladaAloha);
+		
 		
 		Pancho panchoRico = new Pancho("Rico",150,false);
 		Pancho panchoSalsas = new Pancho("Pancho",100,false);
 		Pancho panchoRookie = new Pancho("Rookie",70,false);
 		Pancho panchoVeggie = new Pancho("Veggie",70,false);
+		
+		panchos.add(panchoRico);
+		panchos.add(panchoSalsas);
+		panchos.add(panchoRookie);
+		panchos.add(panchoVeggie);
+		
+		
 		
 		Hamburguesa burguerKelly = new Hamburguesa("Kelly",200,false);
 		Hamburguesa burguerDobleQueso = new Hamburguesa("Doble queso",110,false);
@@ -51,32 +79,48 @@ public class Main {
 		Hamburguesa burguerVeggie = new Hamburguesa("Veggie",110,false);
 		Hamburguesa burguerDobleCarneDobleQueso = new Hamburguesa("Doble carne Doble queso",160,false);
 		
-
+		hamburguesas.add(burguerKelly);
+		hamburguesas.add(burguerDobleQueso);
+		hamburguesas.add(burguerBigWave);
+		hamburguesas.add(burguerJeremy);
+		hamburguesas.add(burguerVeggie);
+		hamburguesas.add(burguerDobleCarneDobleQueso);
 		
-		Combo comboKelly=new Combo("Combo Kelly",320,bebidaGaseosa237,burguerKelly,guarnicionPapas);
-		Combo comboDobleQueso=new Combo("Combo Doble Queso",220,bebidaGaseosa237,burguerDobleQueso,guarnicionPapas);
-		Combo comboJeremy=new Combo("Combo Jeremy",280,bebidaGaseosa237,burguerJeremy,guarnicionPapas);
-		Combo comboBigWave=new Combo("Combo Big Wave",390,bebidaGaseosa237,burguerBigWave,guarnicionPapas);
-		Combo comboVeggie=new Combo("Combo Veggie",220,bebidaGaseosa237,burguerVeggie,guarnicionPapas);
-		Combo comboDobleCarneDobleQueso= new Combo("Combo Doble carne Doble queso",390,bebidaGaseosa237,burguerDobleCarneDobleQueso,guarnicionPapas);
-		Combo comboRico = new Combo ("Combo Rico",325,bebidaGaseosa237,panchoRico,guarnicionPapas);
-		Combo comboRookie = new Combo ("Combo Rookie",230,bebidaGaseosa237,panchoRookie,guarnicionPapas);
-		Combo comboVeggiePancho = new Combo ("Combo Pancho Veggie",85,bebidaGaseosa237,panchoVeggie,guarnicionPapas);
-		Combo comboSalsas = new Combo ("Combo Salsas",230,bebidaGaseosa237,panchoSalsas,guarnicionPapas);
+		combos.add(new Combo("Combo Kelly",320,bebidaGaseosa237,burguerKelly,guarnicionPapas));
+		combos.add(new Combo("Combo Doble Queso",220,bebidaGaseosa237,burguerDobleQueso,guarnicionPapas));
+		combos.add(new Combo("Combo Jeremy",280,bebidaGaseosa237,burguerJeremy,guarnicionPapas));
+		combos.add(new Combo("Combo Big Wave",390,bebidaGaseosa237,burguerBigWave,guarnicionPapas));
+		combos.add(new Combo("Combo Veggie",220,bebidaGaseosa237,burguerVeggie,guarnicionPapas));
+		combos.add(new Combo("Combo Doble carne Doble queso",390,bebidaGaseosa237,burguerDobleCarneDobleQueso,guarnicionPapas));
+		combos.add(new Combo ("Combo Rico",325,bebidaGaseosa237,panchoRico,guarnicionPapas));
+		combos.add(new Combo ("Combo Rookie",230,bebidaGaseosa237,panchoRookie,guarnicionPapas));
+		combos.add(new Combo ("Combo Pancho Veggie",85,bebidaGaseosa237,panchoVeggie,guarnicionPapas));
+		combos.add( new Combo ("Combo Salsas",230,bebidaGaseosa237,panchoSalsas,guarnicionPapas));
+		
 	
-		/*combos.agregarCombo(comboKelly);
-		combos.agregarCombo(comboDobleQueso);
-		combos.agregarCombo(comboJeremy);
-		combos.agregarCombo(comboBigWave);
-		combos.agregarCombo(comboVeggie);
-		combos.agregarCombo(comboDobleCarneDobleQueso);
-		combos.agregarCombo(comboRico);
-		combos.agregarCombo(comboRookie);
-		combos.agregarCombo(comboVeggiePancho);
-		combos.agregarCombo(comboSalsas);*/
-		
-		
+		//Agregamos combos
+		archiProductos.agregarCombo(combos);
 		archiProductos.leerCombo();
+		
+		//Agregamos hamburguesas
+		archiProductos.agregarHamburguesa(hamburguesas);
+		archiProductos.leerHamburguesa();
+		
+		//Agregamos pancho
+		archiProductos.agregarPancho(panchos);
+		archiProductos.leerPancho();
+		
+		//Agregamos ensalada
+		archiProductos.agregarEnsalada(ensaladas);
+		archiProductos.leerEnsalada();
+		
+		//Agregamos guarnicion
+		archiProductos.agregarGuarnicion(guarniciones);
+		archiProductos.leerGuarnicion();
+		
+		//Agregamos bebida
+		archiProductos.agregarBebida(bebidas);
+		archiProductos.leerBebida();
 
 		
 		
