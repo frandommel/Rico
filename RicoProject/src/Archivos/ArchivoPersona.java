3fbfef634f1ee9b4dfcd1a8e6cd951c18afa182f
pvalentinuzzi14 <pvalentinuzzi14@gmail.com>
2020-06-13 18:49:40 -0300

    archivo persona

--- RicoProject/src/Archivos/ArchivoPersona.java
+++ RicoProject/src/Archivos/ArchivoPersona.java
@@ -1,5 +1,9 @@
 package Archivos;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import GestionComercio.ListadoClientes;
 import GestionComercio.ListadoEmpleados;
 import GestionComercio.Persona;
@@ -9,15 +13,24 @@ public class ArchivoPersona {
 		// TODO Auto-generated constructor stub
 	}
 
-	public ListadoEmpleados leerEmpleados() {
-		// TODO Auto-generated method stub
-		return null;
+	public ListadoEmpleados leerEmpleados() throws FileNotFoundException {
+		FileInputStream file = new FileInputStream("empleados.bin");
+		ListadoEmpleados empleados = new ListadoEmpleados();
+		
+		try {
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return empleados;
 	}
 
 	public ListadoClientes leerClientes() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	
 	
 
 	
