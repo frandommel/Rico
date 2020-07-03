fed3b9d2b112427c1d33d2e973e3df302b4842f1
Francisco <pancho.dommel@gmail.com>
2020-07-03 17:49:33 -0300

    Agrego Serial
    Agregue serial version

--- RicoProject/src/Archivos/ArchivoPedidos.java
+++ RicoProject/src/Archivos/ArchivoPedidos.java
@@ -20,6 +20,11 @@ import claseProductos.Producto;
 
 public class ArchivoPedidos	implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public ArchivoPedidos()
 	{
 		
